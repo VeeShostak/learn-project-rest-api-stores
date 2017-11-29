bd5199cb7fb00b7ad287b064150535afa2615344
VeeShostak <VeeShostak@users.noreply.github.com>
2017-11-29 16:10:03 -0500

    add PostgreSQL anvironment var for Heroku deployment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os# postgres://okdcrciecnwvrm:74f9521e77d7a4e9003c4eb5f01e3de458b479b0d97f513351729e81b66f0f51@ec2-54-235-210-115.compute-1.amazonaws.com:5432/dbsi51cf36roti
+
 from flask import Flask
 from flask_restful import Api
 
@@ -10,7 +12,9 @@ from resources.store import Store, StoreList # SQLAlchemy only creates tables th
 
 app = Flask(__name__)
 #SQLAlchemy db will be in root folder of our project (can by MySQL, PostgreSql, oracle...etc)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# in heroku get env db var, if not found, use our sqlite for testing in our local env
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 # FlaskSqlAlchemy know when object changed but not have saved to the db = turn off
 # SQLAlchemy main library itself has a modification tracker
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
